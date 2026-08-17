2b82b47d6dba2ef8c5e05308af9359848d163fe2
korokoro22 <andimuhammadrezkym@gmail.com>
2026-08-17 10:20:03 +0800

    cancel update metadata for each page

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -1,11 +1,11 @@
 import LoginView from "@/views/admin/login/LoginView";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Login | IPPAT Parepare dan sekitarnya",
-  description:
-    "Portal resmi Ikatan Pejabat Pembuat Akta Tanah (IPPAT) Parepare - informasi, layanan, dan berita terkini.",
-};
+// export const metadata: Metadata = {
+//   title: "Login | IPPAT Parepare dan sekitarnya",
+//   description:
+//     "Portal resmi Ikatan Pejabat Pembuat Akta Tanah (IPPAT) Parepare - informasi, layanan, dan berita terkini.",
+// };
 
 const page = () => {
   return <LoginView />;
--- app/admin/notary/page.tsx
+++ app/admin/notary/page.tsx
@@ -1,11 +1,11 @@
 import NotaryView from "@/views/admin/notary/NotaryView";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Admin Notaris | IPPAT Parepare dan sekitarnya",
-  description:
-    "Portal resmi Ikatan Pejabat Pembuat Akta Tanah (IPPAT) Parepare - informasi, layanan, dan berita terkini.",
-};
+// export const metadata: Metadata = {
+//   title: "Admin Notaris | IPPAT Parepare dan sekitarnya",
+//   description:
+//     "Portal resmi Ikatan Pejabat Pembuat Akta Tanah (IPPAT) Parepare - informasi, layanan, dan berita terkini.",
+// };
 
 const page = () => {
   return <NotaryView />;
